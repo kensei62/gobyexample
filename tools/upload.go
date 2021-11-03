44ea295850ff8a6957e6d89deff7b23d267b7e79
warkentyne <kw@mergebase.com>
2021-11-03 12:30:20 -0700

    Added rclone to upload.go and ran build

--- tools/upload.go
+++ tools/upload.go
@@ -15,6 +15,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+
+        "github.com/rclone/rclone"
 )
 
 // guessContentType guesses the HTTP content type appropriate for the given
